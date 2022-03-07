de3122fcc111bfbff0ebdb8bbe9ebc2ed9df411a
Celine Vogel <vogelceline@gmx.de>
2022-03-07 12:13:34 +0100

    Adding Creepypasta to Storybook

---
+++ src/components/Creepypasta.stories.js
@@ -0,0 +1,15 @@
+import Creepypasta from './Creepypasta';
+
+export default {
+  title: 'components/Creepypasta',
+  component: Creepypasta,
+};
+
+const Template = args => <Creepypasta {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Slenderman',
+  text: 'This is the story of Slenderman',
+  wordcount: '1973',
+};
