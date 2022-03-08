7e274f30997d4666891c2a44bd4526645568971c
Celine Vogel <vogelceline@gmx.de>
2022-03-08 10:06:58 +0100

    Installing React-Router

--- src/components/Creepypasta.stories.js
+++ src/components/Creepypasta.stories.js
@@ -10,6 +10,8 @@ const Template = args => <Creepypasta {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   title: 'Slenderman',
+  image:
+    'https://www.creepypasta.com/wp-content/uploads/2010/01/slenderman-1.jpg.webp',
   text: 'This is the story of Slenderman',
   wordcount: '1973',
 };
