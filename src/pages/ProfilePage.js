68736bf71c484cd5d9829a403cec87bd9d940170
Celine Vogel <vogelceline@gmx.de>
2022-03-15 11:19:44 +0100

    Adding media query to profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -8,23 +8,25 @@ export default function ProfilePage() {
         <Link to="/">All</Link>
         <Link to="/spookmarked">Spookmarked</Link>
       </Navigation>
-      <Grid>
-        <Position>
-          <PictureCropper>
-            <ProfilePicture
-              alt="Person that created this profile"
-              height="150px"
-              src="https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
-            />
-          </PictureCropper>
-          <h1>Creeps</h1>
-        </Position>
-        <ProfileInfo>About me</ProfileInfo>
-        <p>
-          Welcome to my spooky scary page! I love everything horror, so I'm
-          excited to be here!
-        </p>
-      </Grid>
+      <Flex>
+        <Wrapper>
+          <Position>
+            <PictureCropper>
+              <ProfilePicture
+                alt="Person that created this profile"
+                height="150px"
+                src="https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+              />
+            </PictureCropper>
+            <ProfileName>Creeps</ProfileName>
+          </Position>
+          <ProfileInfo>About me</ProfileInfo>
+          <p>
+            Welcome to my spooky scary page! I love everything horror, so I'm
+            excited to be here!
+          </p>
+        </Wrapper>
+      </Flex>
     </>
   );
 }
@@ -43,8 +45,12 @@ const Link = styled(NavLink)`
 const Position = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-around;
   margin: 10px 0px 0px 0px;
+`;
+
+const ProfileName = styled.h1`
+  margin-left: 40px;
+  font-size: 40px;
 `;
 
 const PictureCropper = styled.div`
@@ -64,14 +70,23 @@ const ProfilePicture = styled.img`
 `;
 
 const ProfileInfo = styled.h2`
-  margin: 5px 0px -27px 0px;
+  margin: 20px 0px -15px 0px;
   font-family: 'Special Elite';
   letter-spacing: 1px;
   font-size: 23px;
 `;
 
-const Grid = styled.main`
-  display: grid;
-  gap: 20px;
+const Wrapper = styled.section`
+  position: relative;
+  @media (min-width: 500px) {
+    height: 200px;
+    width: 500px;
+  }
+`;
+
+const Flex = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding: 0px 20px 20px 20px;
 `;
