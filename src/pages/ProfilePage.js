2f99bf58c8c7c31e502be93443f7c3be620143dc
Celine Vogel <vogelceline@gmx.de>
2022-03-15 12:36:14 +0100

    Adding bookmarked creepypastas to profile page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,7 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import Card from '../components/Card.js';
 
-export default function ProfilePage() {
+export default function ProfilePage({
+  handleBookmarkClick,
+  bookmarkedCreepypastas,
+}) {
   return (
     <>
       <Navigation>
@@ -18,13 +22,32 @@ export default function ProfilePage() {
                 src="https://images.unsplash.com/photo-1487174244970-cd18784bb4a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
               />
             </PictureCropper>
-            <ProfileName>Creeps</ProfileName>
+            <ProfileName>Creepster</ProfileName>
           </Position>
           <ProfileInfo>About me</ProfileInfo>
           <p>
             Welcome to my spooky scary page! I love everything horror, so I'm
             excited to be here!
           </p>
+          <Header>Spookmarked</Header>
+          {bookmarkedCreepypastas.length > 0 ? (
+            bookmarkedCreepypastas.map(creepypasta => (
+              <Card
+                key={creepypasta.id}
+                id={creepypasta.id}
+                title={creepypasta.title}
+                image={creepypasta.image}
+                wordcount={creepypasta.wordcount}
+                onBookmarkClick={() => handleBookmarkClick(creepypasta.id)}
+                isBookmarked={creepypasta.isBookmarked}
+              />
+            ))
+          ) : (
+            <>
+              <h3>It's ghostly quiet in here...</h3>
+              <p>You need to spookmark a story to see it here!</p>
+            </>
+          )}
         </Wrapper>
       </Flex>
     </>
@@ -49,8 +72,8 @@ const Position = styled.div`
 `;
 
 const ProfileName = styled.h1`
-  margin-left: 40px;
-  font-size: 40px;
+  margin-left: 30px;
+  font-size: 35px;
 `;
 
 const PictureCropper = styled.div`
@@ -74,6 +97,16 @@ const ProfileInfo = styled.h2`
   font-family: 'Special Elite';
   letter-spacing: 1px;
   font-size: 23px;
+`;
+
+const Header = styled.h3`
+  text-align: center;
+  font-size: 18px;
+  padding-top: 2px;
+  margin: 0px -20px 25px -20px;
+  border: 1px solid #4d4d4d;
+  border-left: 0px;
+  border-right: 0px;
 `;
 
 const Wrapper = styled.section`
